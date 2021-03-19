44252b6ac19fa0bed65ff027e61fee3f4386a4e4
SahinM <sahinm@mes.local>
2021-03-19 10:08:10 +0100

    upload the start files on project

---
+++ src/main/java/com/mes/taskmanagementapp/TaskManagementAppApplication.java
@@ -0,0 +1,15 @@
+package com.mes.taskmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TaskManagementAppApplication.class, args);
+		System.out.println("demo");
+	}
+
+}
